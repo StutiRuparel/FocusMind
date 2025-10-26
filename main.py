63de24ea0ead710c8112870e9d489887ae481485
Stuti Ruparel <92007271+StutiRuparel@users.noreply.github.com>
2025-10-26 13:04:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -444,4 +444,6 @@ async def stop_face_tracking():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Use $PORT if it exists, otherwise fall back to 8000 for local dev
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
